e63c9db8d1974d1a1b116afcccda44902843f71e
Delfi-CH <laspitzer@gmx.ch>
2025-05-28 08:44:12 +0200

    changed cover

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -6,7 +6,7 @@ export default function Book({title, author, first_published, editions, cover_id
         <div className={"book-container"}>
             <img
                 className={"book-cover"}
-                src={cover_id !== undefined ? `https://covers.openlibrary.org/b/id/${cover_id}-M.jpg` : "https://openlibrary.org/images/icons/avatar_book-lg.png"}
+                src={cover_id !== undefined ? `https://covers.openlibrary.org/b/id/${cover_id}-M.jpg` : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.fantascienza.com%2Fcatalogo%2Fimg%2Fnocover.png&f=1&nofb=1&ipt=6c621b49513852b2b93067d9f0fff87f31d4303bd9d1082555be35d732395750"}
                 alt={`Cover of the book ${title} written by ${author}`}
             />
             <div className={"book-details"}>
